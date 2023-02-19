c0ca9aaf50c097c6fb26a5b7a929dbbc891f3ce1
Brian Almeida <Almeidabrian003@gmail.com>
2023-02-19 14:25:25 -0500

    Adding first steps for Rust kernel

---
+++ kernel/target/debug/deps/kernel-996e9e8e9325d4be.d
@@ -0,0 +1,5 @@
+/home/name/Projects/OS-Printer/OpenSourcePrinting/kernel/target/debug/deps/kernel-996e9e8e9325d4be: src/main.rs
+
+/home/name/Projects/OS-Printer/OpenSourcePrinting/kernel/target/debug/deps/kernel-996e9e8e9325d4be.d: src/main.rs
+
+src/main.rs:
